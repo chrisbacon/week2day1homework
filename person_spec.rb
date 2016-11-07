2c8d9b0793cc8ec2b84b23df19a06bba99d05ef4
chrisbacon <bacon.chris@gmail.com>
2016-11-07 15:54:15 +0000

    initial commit, tests for Person class written

---
+++ person_spec.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('person')
+
+class TestPerson < MiniTest::Test
+
+    def setup
+        Person.new('Chris', 30)
+    end
+
+    def test_person_has_name
+        assert_equal('Chris', person.name)
+    end
+
+    def test_person_has_age
+        assert_equal(30, person.age)
+    end
+
+end
