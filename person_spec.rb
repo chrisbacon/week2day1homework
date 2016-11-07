b69b0367dcaa62a73165ebea93319b9882d74e6d
chrisbacon <bacon.chris@gmail.com>
2016-11-07 15:55:35 +0000

    Person class written and tested

--- person_spec.rb
+++ person_spec.rb
@@ -5,15 +5,15 @@ require_relative('person')
 class TestPerson < MiniTest::Test
 
     def setup
-        Person.new('Chris', 30)
+        @person = Person.new('Chris', 30)
     end
 
     def test_person_has_name
-        assert_equal('Chris', person.name)
+        assert_equal('Chris', @person.name)
     end
 
     def test_person_has_age
-        assert_equal(30, person.age)
+        assert_equal(30, @person.age)
     end
 
 end
